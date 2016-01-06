07c2d839542db889b8bb96c75d42701e9f7edf5b
Rodrigo Peleias <rodrigo.peleias@preventsenior.com.br>
2016-01-06 19:35:59 -0200

    Capitulo 12: Correção de variavéis de ambiente

--- config/env/test.js
+++ config/env/test.js
@@ -4,7 +4,7 @@ module.exports = {
 	sauceTestName: '@rpeleias Contatooh test',
 	sauceUser: process.env.SAUCE_USERNAME,
 	sauceKey: process.env.SAUCE_ACCESS_KEY,
-	travisJobNumber: process.env.TRAVIS_BUILD_NUMBER,
+	travisJobNumber: process.env.TRAVIS_JOB_NUMBER,
 	travisBuild: process.env.TRAVIS_BUILD_NUMBER,
 	clientID: process.env.CLIENT_ID,
 	clientSecret: process.env.CLIENT_SECRET,
